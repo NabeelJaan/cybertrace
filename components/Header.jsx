4dcafb3c5b33187dbd23d853daa1023de314af2c
Muhammad Nabeel <47273004+NabeelJaan@users.noreply.github.com>
2022-10-01 19:58:36 +0500

    header update

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Header = () => {
+
+    const router = useRouter;
+    function isActive(route){
+        if(route == router.pathname){
+            return "active"
+        }
+        else ""
+    }
+
     return(
         <div className="header-wrapper bg-black py-5">
             <nav className="max-w-1140px mx-auto flex items-center justify-between px-5 xl:px-0">
@@ -12,25 +22,25 @@ const Header = () => {
                     </Link>
                 </div>
                 <div>
-                    <ul>
-                        <Link href="/">
-                            <a className="text-white text-base font-medium font-roboto mr-8">Home</a>
-                        </Link>
-                        <Link href="/about">
-                            <a className="text-white text-base font-medium font-roboto mr-8">About</a>
-                        </Link>
-                        <Link href="/">
-                            <a className="text-white text-base font-medium font-roboto mr-8">Services</a>
-                        </Link>
-                        <Link href="/">
-                            <a className="text-white text-base font-medium font-roboto mr-8">Blog and Press</a>
-                        </Link>
-                        <Link href="/">
-                            <a className="text-white text-base font-medium font-roboto mr-8">Database</a>
-                        </Link>
-                        <Link href="/contact">
-                            <a className="text-white text-base font-medium font-roboto"font-roboto >Contact</a>
-                        </Link>
+                    <ul className="flex items-center">
+                        <li className={isActive('/')}>
+                            <Link href="/"><a className="text-white text-base font-medium font-roboto mr-8">Home</a></Link>
+                        </li>
+                        <li className={isActive('/about')}>
+                            <Link href="/about"><a className="text-white text-base font-medium font-roboto mr-8">About</a></Link>
+                        </li>
+                        <li>
+                            <Link href="#"><a className="text-white text-base font-medium font-roboto mr-8">Services</a></Link>
+                        </li>
+                        <li>
+                            <Link href="#"><a className="text-white text-base font-medium font-roboto mr-8">Blog and Press</a></Link>
+                        </li>
+                        <li>
+                            <Link href="#"><a className="text-white text-base font-medium font-roboto mr-8">Database</a></Link>
+                        </li>
+                        <li className={isActive('/contact')}>
+                            <Link href="/contact"><a className="text-white text-base font-medium font-roboto">Contact</a></Link>
+                        </li>
                     </ul>
                 </div>
             </nav>
