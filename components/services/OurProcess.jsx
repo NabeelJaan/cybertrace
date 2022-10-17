91f22a6bc758f137ed313b2327d6914cf60f0992
Muhammad Nabeel <nabeeljaan666@gmail.com>
2022-10-17 17:13:48 +0500

    services components

---
+++ components/services/OurProcess.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const OurProcess = () => {
+    return(
+        <h1>Our Process</h1>
+    )
+}
+
+export default OurProcess
---
+++ pages/cryptocurrency-fraud-tracing.jsx
@@ -0,0 +1,12 @@
+import Layout from './../components/layout';
+import OurProcess from './../components/services/OurProcess';
+
+export default function cryptocurrencyFraudTracing(){
+    return(
+        <Layout title="cryptocurrency Fraud Tracing">
+
+            <OurProcess />
+
+        </Layout>
+    )
+}
