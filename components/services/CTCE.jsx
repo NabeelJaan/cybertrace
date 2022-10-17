a97f1f922b53a00f312cf2c43de41571e87e28ad
Muhammad Nabeel <nabeeljaan666@gmail.com>
2022-10-17 18:09:38 +0500

    ctce component html

---
+++ components/services/CTCE.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const CTCE = () => {
+    return(
+        <section className="bg-white px-15px py-85px xl:px-0">
+            <div className="max-w-1140px mx-auto">
+                
+            </div>
+        </section>
+    )
+}
+
+export default CTCE
--- pages/cryptocurrency-fraud-tracing.jsx
+++ pages/cryptocurrency-fraud-tracing.jsx
@@ -1,11 +1,14 @@
 import Layout from './../components/layout';
 import OurProcess from './../components/services/OurProcess';
+import CTCE from './../components/services/CTCE';
 
 export default function cryptocurrencyFraudTracing(){
     return(
         <Layout title="cryptocurrency Fraud Tracing">
 
             <OurProcess />
+
+            <CTCE />
 
         </Layout>
     )
